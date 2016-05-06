25d2eaf1a18def320406138367563d98af5986ce
Charles Childers <crc@forthworks.com>
2016-05-06 18:00:35 +0000

    rewrite parable backend

--- pl0-parable.py
+++ pl0-parable.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
-# Copyright (c) 2012 Michal J Wallace. <http://www.michaljwallace.com/>
-# Copyright (c) 2012, 2016 Charles R Childers
+# Copyright (c) 2012 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,16 +21,11 @@
 # THE SOFTWARE.
 #
 
-"""
-This translates the pl0 syntax tree to the equivalent
-representation in parable ( http://forthworks.com/parable )
-"""
-from pl0_node_visitor import StackingNodeVisitor
-import sys
-import pl0_parser
-import StringIO
 import os
-import types
+import sys
+#import StringIO
+import pl0_parser
+from pl0_node_visitor import StackingNodeVisitor
 
 # AST->parable translator for operators
 ops = {
@@ -50,259 +44,196 @@ rel_ops = {
     'E'      : 'eq?',
     'NE'     : '-eq?',
 }
-
-UNKNOWN, VAR, CONST, PROCEDURE = range(4)
-CATEGORY = "UNKNOWN VAR CONST PROCEDURE".split()
-
 
-class ParableTranspiler(StackingNodeVisitor):
-
-    stacksize = 256 # cells
-
+class Compiler(StackingNodeVisitor):
+
     def __init__(self):
-        super(ParableTranspiler, self)
-        # negative signs are not included in number tokens,
-        # because a lexer cannot distinguish the "-1" in
-        # the expression "x-1" from "-1" as a negative numer.
-        #
-        # the parser does this as part of the "expression"
-        # rule, so "-1" is parsed as "-(1)". As an optimization,
-        # we set the following flag in accept_expression when
-        # we can see that the value between the parens is a
-        # numeric literal. Then we can emit a negative literal
-        # rather than emittting code to multiply the positive
-        # literal by -1.
-        self.negate = False
-        self.name_op = "@"
-        self.proc_path = []    # for nested procedure defs
-        self.local_defs = {}   # symbol table for top level / current proc
-        self.scope = []        # list of symbol tables for lexical scope
-        self.scope.append(self.local_defs)
-
-    def local_vars(self):
-        """
-        returns a list of names of variables to preserve
-        when calling functions recursively
-        """
-        return [ key for (key, (kind, _)) in self.local_defs.items()
-                 if kind == VAR ]
-
-    def lookup(self, name):
-        for frame in reversed( self.scope ):
-            if name in frame :
-                return frame[ name ]
-            else: pass
-        else: raise LookupError( "name not found: %s. scope is %r"
-                                 % ( name, self.scope ))
-
-    def visit( self, node ):
-        """like visit_node but works with generators"""
-        result = self.visit_node( node )
-        if isinstance( result, types.GeneratorType ):
-            result = "\n".join( result )
-        return result
+        super(Compiler, self).__init__()
+        self.label_id = 0
 
-    #-- simple numbers -----------------
+    def intermediate_label(self, hint = ''):
+        self.label_id += 1
+        return 't_' + hint + '_' + repr(self.label_id)
+
+    def generate(self, node):
+        self.push()
+        result = self.visit_node(node)
+        return [self.pop(), result]
+
+    def accept_variables(self, *node):
+        for var in node[1:]:
+            # Generate a unique name for the variable
+            variable_name = self.intermediate_label('var_' + var[1])
 
-    def accept_number(self, nid, value):
-        if self.negate:
-            print "#-{0}".format(value),
-            self.negate = False
+            # Save the unique name for loading this variable in the future.
+            self.stack[-1].update(var[1], variable_name)
+
+            # Allocate static storage space for the variable
+            print("'" + variable_name + "' var")
+#            print "    0"
+
+    def accept_constants(self, *node):
+        for var in node[1:]:
+            self.stack[-1].define(var[1], var[2])
+
+    def accept_procedures(self, *node):
+        for proc in node[1:]:
+            # Generate a unique name for the procedure
+            proc_name = self.intermediate_label('proc_' + proc[1])
+
+            # Save the unique procedure name on the lexical stack
+            self.stack[-1].declare(proc[1], proc_name)
+
+            # Define a new lexical scope
+            self.push()
+
+            # Generate any static storage required by the procedure
+            print("\"Procedure " + proc[1] + "\"")
+            self.visit_expressions(proc[2][1:3])
+
+            # Generate the code for the procedure
+            print("[ ] '" + proc_name + "' :")
+            print("[")
+            self.visit_node(proc[2][4])
+            print("] '" + proc_name + "' :")
+
+            # Finished with lexical scope
+            self.pop()
+
+    def accept_program(self, *node):
+#        print "JMP main"
+        print("'Output' var\n&Output pop drop")
+
+        block = node[1]
+        self.visit_expressions(block[1:4])
+
+        print("[")
+        self.visit_node(block[4])
+        print("] 'main' :")
+        print('main Output')
+
+    def accept_while(self, *node):
+        top_label = self.intermediate_label("while_start")
+        bottom_label = self.intermediate_label("while_end")
+
+        condition = node[1]
+        loop = node[2]
+
+        print("[")
+#        print top_label + ":"
+        # Result of condition is on top of stack
+
+#        print "\tJE " + bottom_label
+
+        self.visit_node(loop)
+
+#        print "\tJMP " + top_label
+#        print bottom_label + ":"
+        self.visit_node(condition)
+        print("] while")
+
+
+    def accept_if(self, *node):
+        false_label = self.intermediate_label("if_false")
+
+        condition = node[1]
+        body = node[2]
+
+        self.visit_node(condition)
+        print("[")
+#        print "\tJE " + false_label
+
+        self.visit_node(body)
+
+        print("] if-true")
+
+#        print false_label + ":"
+
+    def accept_condition(self, *node):
+        operator = node[2]
+        lhs = node[1]
+        rhs = node[3]
+
+        self.visit_node(lhs)
+        self.visit_node(rhs)
+
+#        print "\tCMP" + operator
+        print(" " + rel_ops[operator])
+
+    def accept_set(self, *node):
+        name = node[1][1]
+
+        self.visit_node(node[2])
+
+        assign_to = node[1][1]
+        defined, value, level = self.find(assign_to)
+
+        if defined != 'VARIABLE':
+            raise NameError("Invalid assignment to non-variable " + assign_to + " of type " + defined)
+
+        print(" !" + str(value))
+
+    def accept_call(self, *node):
+        defined, value, level = self.find(node[1])
+
+        if defined != 'PROCEDURE':
+            raise NameError("Expecting procedure but got: " + defined)
+
+        print(" |" + value)
+
+    def accept_term(self, *node):
+        self.visit_node(node[1])
+
+        for term in node[2:]:
+            self.visit_node(term[1])
+
+            if term[0] == 'TIMES':
+                print(" *")
+            elif term[0] == 'DIVIDES':
+                print(" / floor")
+
+    def accept_expression(self, *node):
+        # Result of this expression will be on the top of stack
+        self.visit_node(node[2])
+
+        for term in node[3:]:
+            self.visit_node(term[1])
+
+            if term[0] == 'PLUS':
+                print(" +")
+            elif term[0] == 'MINUS':
+                print(" -")
+
+        if node[1] == 'MINUS':
+            print(" -1 *")
+
+    def accept_print(self, *node):
+        self.visit_node(node[1])
+        print(" &Output push")
+#        print "\tPOP"
+
+    def accept_number(self, *node):
+        print(" #" + repr(node[1]))
+
+    def accept_name(self, *node):
+        defined, value, level = self.find(node[1])
+
+        if defined == 'VARIABLE':
+            print(" @" + value)
+        elif defined == 'CONSTANT':
+            print(" " + str(value))
         else:
-            sys.stdout.write(" #")
-            print value,
-
-    # logically, print ("!") would come much later
-    # but i'm putting these in implementation order,
-    # and i want this up front so i can see the
-    # results of running the code.
-    def accept_print(self, nid, expr):
-        self.visit( expr )
-        print 'PL/0.display',
-
-    #-- expressions --------------------
-
-    # example: a + b * c + d * e + f
-    # becomes: a b c * + d e * + f +
-
-    # term = factor {("*"|"/") factor}.
-    def accept_term( self, nid, *factors_tup ):
-        factors = list( factors_tup )
-        self.visit( factors.pop( 0 ))
-        for operator, operand in factors:
-            self.visit( operand )
-            print ops[ operator ],
-
-    # expression = [ "+"|"-"] term { ("+"|"-") term}.
-    def accept_expression(self, nid, sign, *terms_tup):
-        terms = list( terms_tup )
-
-        # handle negative numbers and unary minus
-        negate_value = False
-        negate_after = False
-        if sign == "MINUS":
-            assert len(terms_tup) == 1
-            if terms_tup[0][1][0] == "NUMBER":
-                self.negate = True
-            else:
-                negate_after = True
-
-        # generate the normal expression
-        for node in terms:
-            if node[0]=="TERM":
-                self.visit(node)
-            else:
-                operator, term = node
-                self.visit( term )
-                print ops[ operator ],
-
-        if negate_after:
-            print "#-1 *",
-
-    #-- named constants ----------------
-
-    def accept_define(self, nid, name, value):
-        self.local_defs[ name ] = (CONST, value)
-
-    def accept_name(self, nid, name):
-        category, value = self.lookup( name )
-        if category == CONST:
-            sys.stdout.write(" #{0} ".format(value))
-        elif category == VAR:
-            sys.stdout.write(' ')
-            sys.stdout.write(self.name_op)
-            if len(self.scope) > 1:
-                sys.stdout.write("~" + value)
-            else:
-                sys.stdout.write(value)
-            sys.stdout.write(' ')
-        else:
-            raise Exception("unhandled name: (%s:%s) . scope is: %r"
-                            % [CATEGORY[category], value,
-                               self.scope])
-
-
-    #-- named variables & assignment ---
-
-    def accept_variables(self, nid, *names):
-        if len(self.scope) > 1:
-            prefix = "~"
-        else:
-            prefix = ""
-        sys.stdout.write('[ ')
-        for nid, name in names:
-            print "'" + prefix + name + "' ",
-            self.local_defs[ name ] = (VAR, name)
-        print '] ::'
-        if prefix == "": sys.stdout.write('\n')
-
-    def accept_set(self, nid, name, expr):
-        self.visit( expr )
-        self.name_op = "!"
-        self.visit( name )
-        self.name_op = "@"
-
-
-    #-- flow control -------------------
-
-    def accept_odd(self, nid, expr):
-        self.visit( expr )
-        print "odd?",
-
-    def accept_condition(self, nid, lhs, rel, rhs):
-        self.visit( lhs )
-        self.visit( rhs )
-        sys.stdout.write(' ' + rel_ops[ rel ] + ' ')
-
-    def accept_if(self, nid, cond, stmt):
-        self.visit( cond )
-        sys.stdout.write(" [ ")
-        self.visit( stmt )
-        sys.stdout.write(" ] if-true ")
-
-    def accept_while(self, nid, cond, stmt):
-        self.visit( cond )
-        print " [ [",
-        self.visit( stmt )
-        self.visit( cond )
-        print "] while ] if-true",
-
-
-    #-- procedures ---------------------
-
-    def accept_program(self, nid, block):
-
-        (blocknid, procs, consts, vars, stmt) = block
-        print "'*Output' var"
-        print "[ &*Output push ] 'PL/0.display' :"
-        print '"----------------------------------------------------------------"'
-        self.visit_expressions([procs, consts, vars])
-        sys.stdout.write("\n[ ")
-        self.visit(stmt)
-        sys.stdout.write(" ] 'run' :\n")
-        print '"----------------------------------------------------------------"'
-        print "run\n*Output"
-
-
-    # for recursion, we need to maintain a stack
-    def accept_procedure(self, nid, name, block):
-
-        (blocknid, procs, consts, vars, stmt) = block
-        self.proc_path.append(name)
-        self.scope.append(self.local_defs)
-
-        # enable recursion
-        sys.stdout.write("[ ] '" + name + "' :\n")
-
-        self.visit_expressions([procs, consts, vars])
-        sys.stdout.write("[ ")
-        self.visit(stmt)
-        sys.stdout.write(" ] '" + name + "' :\n")
-
-        if self.scope > 1:
-            sys.stdout.write('[ ')
-            for v in self.local_defs:
-                sys.stdout.write("'~" + v + "' ")
-            sys.stdout.write('] hide-words\n')
-
-        self.proc_path.pop()
-        self.scope.pop()
-        self.local_defs = self.scope[-1]
-
-    def accept_call(self, nid, name):
-
-        # this detects simple recursion
-        #TODO: full recursion check
-        #TODO: tail call optimization
-        #TODO: only push/pop shadowed variables
-        recursive = name in self.proc_path
-
-        def call(): print name,
-
-        keep = self.local_vars()
-
-        if recursive:
-            sys.stdout.write(' [ ')
-            for ident in keep:
-                 sys.stdout.write(' &' + ident)
-            sys.stdout.write(' ] [ ')
-            call()
-            sys.stdout.write(' ] invoke<preserving> ')
-#            for ident in reversed( keep ):
-#                 sys.stdout.write(' !' + ident)
-        else:
-            call()
+            raise NameError("Invalid value name " + node[1] + " of type " + defined)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print 'PL/0 to Parable Transpiler'
-        print 'Usage:'
-        print '    ./pl0-parable.py input >output'
+        print('PL/0 to Parable Transpiler')
+        print('Usage:')
+        print('    ./pl0-parable.py input >output')
     else:
         code = open(sys.argv[1], 'r').read()
         parser = pl0_parser.Parser()
         parser.input(code)
         program = parser.p_program()
-        compiler = ParableTranspiler()
-        compiler.visit_node(program)
-
+        compiler = Compiler()
+        compiler.generate(program)
