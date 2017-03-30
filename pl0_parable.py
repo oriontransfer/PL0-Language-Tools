4ad6f2d1d5a63cd73ed7138f9015cac7661f06a4
Charles Childers <crc@forthworks.com>
2017-03-30 16:55:34 +0000

    work on fixing tests

--- pl0_parable.py
+++ pl0_parable.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #
 # Copyright (c) 2012 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
 # Copyright (c) 2012 Michal J Wallace. <http://www.michaljwallace.com/>
